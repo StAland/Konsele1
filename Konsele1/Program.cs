c8337458f1026cd3934d4f20eb8f7f1f62704860
StAland <steffen.aland@gmx.de>
2025-01-13 08:46:44 +0100

    Add project files.

---
+++ Konsele1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+namespace Konsole1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            else
+            {
+                foreach (var arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
