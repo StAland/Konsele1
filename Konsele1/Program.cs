1d6abb77426ddf10867afa8e43b406fac4d05290
StAland <steffen.aland@gmx.de>
2025-01-13 10:55:06 +0100

    Nutzereingabe und TryParse

--- Konsele1/Program.cs
+++ Konsele1/Program.cs
@@ -9,42 +9,30 @@ namespace Konsole1
     {
         static void Main(string[] args)
         {
+            var zahl1string = NutzereingabeAbfragen("Bitte eine Zahl eingeben");
+            var zahl2string = NutzereingabeAbfragen("Bitte eine zweite Zahl eingeben");
+            int zahl1;
+            int zahl2;
+            if(int.TryParse(zahl1string, out zahl1) && int.TryParse(zahl2string, out zahl2))
+            {
+                int zahl3 = zahl1 + zahl2;
+                Console.WriteLine($"{zahl1} + {zahl2} = {zahl3}");
+            }
+            else
+            {
+                Console.WriteLine("Keine Zahlen eingegeben");
+            }
+        }
 
-            sbyte zahl1 = 127;
-            bool geklappt = sbyte.TryParse("Hallo", out zahl1);
-            short zahl2 = 32767;
-            int zahl3 = 214748364;
-            
-            long zahl4 = 9223372036854775807;
-            byte zahl5 = 255;
-            ushort zahl6 = 65535;
-            uint zahl7 = 4294967295;
-            ulong zahl8 = 18446744073709551615;
-
-            float zahl9 = 1.4f;
-            float zahl10 = 10000000000000000000000000f;
-            float zahl11 = 10000000000000000000000020f;
-            double zahl12 = 10000000000000000000000000d;
-            double zahl13 = 10000000000000000000000020d;
-            decimal zahl14 = 10000000000000000000000000m;
-            decimal zahl15 = 10000000000000000000000002m;
-            var gleich = zahl14 == zahl15;
-
-            bool var1 = false;
-            bool var2 = true;
-
-            bool var3 = var1 && var2;
-            bool var4 = var2 || var1;
-
-            string var5 = "Hallo \t \\ \n \" Hallo test";
-            char var6 = '\n';
-            string var7 = $" {var1} && {var2} = {var3}";
-
-            DateTime zeit = DateTime.Parse("2024-12-24");
-            TimeSpan zeitspanne = TimeSpan.FromDays(2);
-
-            Console.WriteLine(zahl2);
-
+        public static string NutzereingabeAbfragen(string eingabe)
+        {
+            Console.WriteLine(eingabe);
+            var zahl = Console.ReadLine();
+            if (zahl != null)
+            {
+                return zahl;
+            }
+            return "";
         }
     }
 }
