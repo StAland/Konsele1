8e677b13a472d89bcc8c46ad47bb3369b412123d
StAland <steffen.aland@gmx.de>
2025-01-16 08:30:40 +0100

    LINQ Select, First

--- Konsele1/Program.cs
+++ Konsele1/Program.cs
@@ -13,41 +13,18 @@ namespace Konsole1
 
         static void Main(string[] args)
         {
-            var kisten = new List<Kiste>();
-            kisten.Add(new Kiste(15));
-            kisten.Add(new Kiste(55));
-            kisten.Add(new Kiste(40));
-            kisten.Add(new Kiste(70));
-            kisten.Add(new Kiste(115));
-            kisten.Add(new Kiste(36));
-            kisten.Add(new Kiste(80));
-            kisten.Add(new Kiste(7));
-            kisten.Add(new Kiste(23));
-            kisten.Add(new Kiste(85));
+            var bruechedict = new Dictionary<int, int>();
+            bruechedict.Add(5, 3);
+            bruechedict.Add(7, 2);
+            bruechedict.Add(2, 9);
+            bruechedict.Add(4, 3);
 
-            var kistenGreater50 = new List<Kiste>();
-            foreach (var kiste in kisten)
+            var brueche = bruechedict.Select(item => new Bruch(item.Key, item.Value));
+            var bruchMitNenner3 = brueche.First(item => item.Nenner == 3);
+            foreach (var item in brueche)
             {
-                if (kiste.Volumen >= 50)
-                {
-                    kistenGreater50.Add(kiste);
-                }
+                Console.WriteLine(item);
             }
-            var kisteGroesser1000 = kisten.Any(kiste => kiste.Volumen > 1000);
-            var kistensmaller50 = from kiste in kisten where kiste.Volumen < 50 select kiste;
-            foreach (var kiste in kistensmaller50)
-            {
-                Console.WriteLine(kiste.Volumen);
-            }
-            var kistengroeser50 = kisten.Where(kiste => kiste.Volumen >= 50);
-            foreach (var kiste in kistengroeser50)
-            {
-                Console.WriteLine(kiste.Volumen);
-            }
-            
-
-        }
-       
-
+        }     
     }
 }
