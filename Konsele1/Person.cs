feacbc76a187251e21b7fb3dbd769339628a1daf
StAland <steffen.aland@gmx.de>
2025-01-20 11:45:05 +0100

    Xml Serialisieren

---
+++ Konsele1/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Konsele1
+{
+    [XmlType("person")]
+    public class Person
+    {
+        [XmlAttribute("id")]
+        public string ID { get; set; }
+        [XmlElement("nachname")]
+        public string Name { get; set; }
+        [XmlElement("vorname")]
+        public string Vorname { get; set; }
+
+
+        public static List<Person> Personen
+        {
+            get
+            {
+
+                return
+                [
+                    new Person("1", "Mustermann", "Max"),
+                    new Person("2", "Musterfrau", "Marta")
+                ];
+            }
+        }
+        public Person()
+        {
+
+        }
+        public Person(string id, string name, string vorname)
+        {
+            ID = id;
+            Name = name;
+            Vorname = vorname;
+        }
+        
+    }
+}
--- Konsele1/Program.cs
+++ Konsele1/Program.cs
@@ -7,6 +7,7 @@ using Konsele1;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace Konsole1
 {
@@ -15,34 +16,12 @@ namespace Konsole1
 
         static void Main(string[] args)
         {
-            var wurzel = XElement.Load("Personen.xml");
-            var xmlPersonen = wurzel.Descendants("Person");
-            foreach (var person in xmlPersonen)
+            var serialisierer = new XmlSerializer(typeof(List<Person>));
+            using (var fileStream = new FileStream("Persons.xml", FileMode.Create))
             {
-                var id = person.Attribute("Id").Value;
-                var vorname = person.Descendants("Vorname").First().Value;
-                var nachname = person.Descendants("Nachname").First().Value;
-                Console.WriteLine($"ID: {id}, {vorname} {nachname}");
+                serialisierer.Serialize(fileStream, Person.Personen);
             }
-
-            var wurzelNeu = new XElement("Personen",
-                new XElement("Person",
-                    new XAttribute("Id", "1"),
-                    new XElement("Vorname", "Robert"),
-                    new XElement("Nachname", "Schiefele")
-                ),
-                new XElement("Person",
-                    new XAttribute("Id", "2"),
-                    new XElement("Vorname", "Max"),
-                    new XElement("Nachname", "Mustermann")
-                ),
-                new XElement("Person",
-                    new XAttribute("Id", "3"),
-                    new XElement("Vorname", "Marta"),
-                    new XElement("Nachname", "Musterfrau")
-                )
-                );
-            wurzelNeu.Save("Personen2.xml");
-        }     
+        }
+            
     }
 }
