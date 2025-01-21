06be57f0cb7a80d5c125249539b541aa0428ce68
StAland <steffen.aland@gmx.de>
2025-01-21 11:50:31 +0100

    Progress

--- Konsele1/Program.cs
+++ Konsele1/Program.cs
@@ -16,33 +16,22 @@ namespace Konsole1
 
         static async Task Main(string[] args)
         {
-            Task<int> task1 = SimulateCalculationAsync(5); // Quadrieren einer Zahl
-            Task<string> task2 = SimulateConversionAsync("Hallo Welt"); // String in Großbuchstaben
-            Task task3 = SimulateMessageWritingAsync("Nachricht wird geschrieben..."); // Schreiben einer Nachricht
-
-            await Task.WhenAll(task1, task2, task3);
-
-            Console.WriteLine($"Berechnungsergebnis: {await task1}");
-            Console.WriteLine($"Konvertierungsergebnis: {await task2}");
+            IProgress<int> progress = new Progress<int>(percent =>
+            {
+                Console.WriteLine($"Fortschritt: {percent}");
+            });
+            await SimulateProgress(progress);
+            Console.WriteLine("Bearbeitung abgeschlossen");
         }
 
-
-        static async Task<int> SimulateCalculationAsync(int number)
+        static async Task SimulateProgress(IProgress<int> progress)
         {
-            await Task.Delay(1000); // Simulierte Verzögerung
-            return number * number;
+            for (int i = 0; i <= 100; i+=5)
+            {
+                await Task.Delay(500);
+                progress.Report(i);
+            }
         }
 
-        static async Task<string> SimulateConversionAsync(string input)
-        {
-            await Task.Delay(2000); // Simulierte Verzögerung
-            return input.ToUpper();
-        }
-
-        static async Task SimulateMessageWritingAsync(string message)
-        {
-            await Task.Delay(3000); // Simulierte Verzögerung
-            Console.WriteLine(message);
-        }
     }
 }
