d8001d12df4119ae104cb00eb904d296581f4102
StAland <steffen.aland@gmx.de>
2025-04-15 09:51:45 +0200

    Binary Trree

---
+++ SortierAlgorithmen/QuickSorter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortierAlgorithmen
+{
+    public class QuickSorter
+    {
+        public static void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(array, low, high);
+
+                // Rekursiv QuickSort auf den linken und rechten Teil anwenden
+                QuickSort(array, low, pi - 1);
+                QuickSort(array, pi + 1, high);
+            }
+        }
+
+        private static int Partition(int[] array, int low, int high)
+        {
+            // Pivot-Element (wir wählen das letzte Element als Pivot)
+            int pivot = array[high];
+            int i = (low - 1);  // Index des kleineren Elements
+
+            for (int j = low; j < high; j++)
+            {
+                // Wenn das aktuelle Element kleiner oder gleich dem Pivot ist
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    // Elemente tauschen
+                    Swap(ref array[i], ref array[j]);
+                }
+            }
+
+            // Das Pivot-Element an seine endgültige Position bringen
+            Swap(ref array[i + 1], ref array[high]);
+            return i + 1; // Index des Pivot-Elements
+        }
+
+        private static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
---
+++ Trees/BinaryTree.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class BinaryTree
+    {
+        public TreeNode Root;
+
+        public void Insert(int value)
+        {
+            Root = InsertRecursive(Root, value);
+        }
+
+        private TreeNode InsertRecursive(TreeNode node, int value)
+        {
+            if (node == null)
+                return new TreeNode(value);
+
+            if (value < node.Value)
+                node.Left = InsertRecursive(node.Left, value);
+            else if (value > node.Value)
+                node.Right = InsertRecursive(node.Right, value);
+
+            return node;
+        }
+
+        public bool Search(int value)
+        {
+            return SearchRecursive(Root, value);
+        }
+
+        private bool SearchRecursive(TreeNode node, int value)
+        {
+            if (node == null)
+                return false;
+
+            if (value == node.Value)
+                return true;
+            else if (value < node.Value)
+                return SearchRecursive(node.Left, value);
+            else
+                return SearchRecursive(node.Right, value);
+        }
+
+        public void Delete(int value)
+        {
+            Root = DeleteRecursive(Root, value);
+        }
+
+        private TreeNode DeleteRecursive(TreeNode node, int value)
+        {
+            if (node == null) return null;
+
+            if (value < node.Value)
+            {
+                node.Left = DeleteRecursive(node.Left, value);
+            }
+            else if (value > node.Value)
+            {
+                node.Right = DeleteRecursive(node.Right, value);
+            }
+            else
+            {
+                // Fall 1: Kein Kind
+                if (node.Left == null && node.Right == null)
+                    return null;
+
+                // Fall 2: Ein Kind
+                if (node.Left == null)
+                    return node.Right;
+                else if (node.Right == null)
+                    return node.Left;
+
+                // Fall 3: Zwei Kinder
+                TreeNode minNode = FindMin(node.Right);
+                node.Value = minNode.Value;
+                node.Right = DeleteRecursive(node.Right, minNode.Value);
+            }
+
+            return node;
+        }
+
+        private TreeNode FindMin(TreeNode node)
+        {
+            while (node.Left != null)
+                node = node.Left;
+            return node;
+        }
+
+        public void PrintInOrder()
+        {
+            PrintInOrderRecursive(Root);
+            Console.WriteLine();
+        }
+
+        private void PrintInOrderRecursive(TreeNode node)
+        {
+            if (node != null)
+            {
+                PrintInOrderRecursive(node.Left);
+                Console.Write($"{node.Value} ");
+                PrintInOrderRecursive(node.Right);
+            }
+        }
+    }
+
+}
